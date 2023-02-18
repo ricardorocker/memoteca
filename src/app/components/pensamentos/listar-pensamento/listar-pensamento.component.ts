90ba1b0ae0cb17587280dbc69436f2ad4fc7dcd4
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-18 17:35:11 -0300

    ngClass

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class ListarPensamentoComponent {
   listaPensamentos = [
     {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
+      conteudo: 'Não andeis ansiosos por coisa alguma; antes em tudo sejam os vossos pedidos conhecidos diante de Deus pela oração e súplica com ações de graças; e a paz de Deus, que excede todo o entendimento, guardará os vossos corações e os vossos pensamentos em Cristo Jesus.',
+      autoria: 'Filipenses 4:6-7',
       modelo: 'modelo3'
     },
     {
