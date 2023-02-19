b54b4d1c271944242ac577e8c36239b70d66462c
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-18 23:34:41 -0300

    FormBuilder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -28,7 +28,8 @@ import { EditarPensamentoComponent } from './components/pensamentos/editar-pensa
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
@@ -11,16 +12,27 @@ import { Pensamento } from './../pensamento';
 })
 export class CriarPensamentosComponent {
 
-  pensamento : Pensamento = {
+  pensamento: Pensamento = {
     conteudo: '',
     autoria: '',
     modelo: 'modelo1'
   }
 
+  formulario!: FormGroup;
+
   constructor(
     private service: PensamentoService,
-    private router: Router
-    ) {}
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: [''],
+      modelo: ['modelo1']
+    })
+  }
 
   criarPensamento() {
     this.service.criar(this.pensamento).subscribe(() => {
