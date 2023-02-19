8d711ff86f57b7db241372008c86588468883d12
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-18 22:45:46 -0300

    Componente de editar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path: '', redirectTo: 'listarPensamento', pathMatch: 'full'},
   {path: 'criarPensamento', component: CriarPensamentosComponent},
   {path: 'listarPensamento', component: ListarPensamentoComponent},
-  {path: 'pensamentos/excluirPensamento/:id', component: ExcluirPensamentoComponent}
+  {path: 'pensamentos/excluirPensamento/:id', component: ExcluirPensamentoComponent},
+  {path: 'pensamentos/editarPensamento/:id', component: EditarPensamentoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CriarPensamentosComponent } from './components/pensamentos/criar-pensam
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
 import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pen
     CriarPensamentosComponent,
     ListarPensamentoComponent,
     PensamentoComponent,
-    ExcluirPensamentoComponent
+    ExcluirPensamentoComponent,
+    EditarPensamentoComponent
   ],
   imports: [
     BrowserModule,
