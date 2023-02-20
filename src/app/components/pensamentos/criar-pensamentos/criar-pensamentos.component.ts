8c080c7d8de89a0663f3ef8dcf9bd31c19012184
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-20 16:31:53 -0300

    Exibindo mensagens de erro

--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -28,7 +28,7 @@ export class CriarPensamentosComponent {
   }
 
   criarPensamento() {
-    console.log(this.formulario);
+    console.log(this.formulario.get('autoria')?.errors);
     if (this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento']);
