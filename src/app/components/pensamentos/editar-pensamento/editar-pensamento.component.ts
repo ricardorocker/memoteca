f564ee71e02087c5e466f447166d72801129974f
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-20 19:19:51 -0300

    Formulário reativo no componente editarPensamento

--- src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,34 +11,43 @@ import { Pensamento } from '../pensamento';
   styleUrls: ['./editar-pensamento.component.css']
 })
 export class EditarPensamentoComponent implements OnInit {
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
+  form!: FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-      this.pensamento = pensamento;
+      this.form = this.formBuilder.group({
+        id: [pensamento.id],
+        conteudo: [pensamento.conteudo, Validators.compose([Validators.required, Validators.minLength(3)])],
+        autoria: [pensamento.autoria, Validators.compose([Validators.required, Validators.minLength(3)])],
+        modelo: [pensamento.modelo]
+      })
     })
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(() => {
+    this.service.editar(this.form.value).subscribe(() => {
       this.router.navigate(['/listarPensamento']);
     })
   }
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
+  }
+
+  onDisableButton() {
+    if (this.form.valid) {
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
+    }
   }
 
 }
