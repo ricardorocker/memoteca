3577fbc31f9f10fefb9794981e3464871b44abaf
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-20 16:13:47 -0300

    pattern e minLenght

--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -21,8 +21,8 @@ export class CriarPensamentosComponent {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]],
-      autoria: ['', [Validators.required]],
+      conteudo: ['', Validators.compose([Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autoria: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       modelo: ['modelo1']
     })
   }
